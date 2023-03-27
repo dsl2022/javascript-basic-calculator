fd2fd015094dc1bca967a91b5469f03d7bd63c46
dsl2022 <2015rpro@gmail.com>
2023-03-27 16:38:21 -0400

    updated

--- script.js
+++ script.js
@@ -2,17 +2,36 @@ function calculate(operation) {
     const input1 = document.getElementById("input1").value;
     const input2 = document.getElementById("input2").value;
     let result;
-    
-    //TODO: uncomment the following code, and validate the numbers are not empty
-    // if (input1 === "" || input2 === "") {
-    //     alert("Please enter both numbers.");
-    //     return;
-    // }
+
+    if (input1 === "" || input2 === "") {
+        alert("Please enter both numbers.");
+        return;
+    }
 
     const num1 = parseFloat(input1);
     const num2 = parseFloat(input2);
-    //TODO: your code here, add the 2 numbers
-    
+
+    switch (operation) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                alert("Cannot divide by zero.");
+                return;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            alert("Invalid operation.");
+            return;
+    }
 
     document.getElementById("result").textContent = result;
 }
